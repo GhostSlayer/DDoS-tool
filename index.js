2fbea27f482670944a0be4cdffc2f3782f95b3e2
RiversideRocks <59586759+RiversideRocks@users.noreply.github.com>
2021-02-08 15:59:14 -0500

    l33ter

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
+console.log("Loading in hacks... 10% done..")
+console.log("Loading in hacks... 32% done..")
+console.log("Loading in hacks... 85% done..")
+console.log("Loading in hacks... 100% done..")
+
+
 console.log('Starting ddos attack')
 
 function ddos(webiste) {
   webiste = 'riverside.rocks'
-  console.log(`Packet sent to ${webiste}`)
+  console.log(`64 Packets sent to ${webiste} [200]`)
 }
 
 setInterval(ddos, 0);
